4f6b2602d8db4bb8270c20a9316e8756bdbd0b24
SSF gRED bCB Comp Genetics acct - <compsci@nl004.ib.ghpc1.sc1.roche.com>
2018-12-03 17:03:41 -0800

    note about meadow version compatiblity added

--- modules/Bio/EnsEMBL/Hive/Meadow/SLURM.pm
+++ modules/Bio/EnsEMBL/Hive/Meadow/SLURM.pm
@@ -10,7 +10,7 @@
 
 =head1 Compatibility 
    
-    Module version 5.3 is compatible with SLURM version 17.11.11 
+    Module version 5.3 is compatible with SLURM version 17.11.11 and eHive Meadow v5.3
 
 =head1 LICENSE
 
