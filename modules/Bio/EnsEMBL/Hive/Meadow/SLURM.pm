875d3ebd5d1e2d67e841d4fe7c75d5e95b511d9a
Jan Vogel <tweep@users.noreply.github.com>
2018-12-08 09:09:56 -0800

    Update SLURM.pm
    Fixed result reports

--- modules/Bio/EnsEMBL/Hive/Meadow/SLURM.pm
+++ modules/Bio/EnsEMBL/Hive/Meadow/SLURM.pm
@@ -284,8 +284,7 @@ sub parse_report_source_line {
 
         next unless $job_name =~ m/batch/; 
 
-        $mem_used =~ s/K$//; 
-        $mem_used /= 1024; # convert to kb to meg 
+        $mem_used =~ s/M$//; # results are reported in Megabytes
 
      
         my $cause_of_death = get_cause_of_death($state) ; 
